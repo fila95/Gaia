7fe91606d2f0d269f0d45db22c5ecdafa3112c12
Giovanni Filaferro <g.filaferro@nanotek.me>
2018-11-14 15:47:27 +0100

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ cd /var/srvcs/services/definitions
 
 for filename in *.service; do
 	noext=${filename%%.*}
-	echo $noext
 
 	sudo cp $filename /lib/systemd/system/$filename
 	sudo chmod 644 /lib/systemd/system/$filename
