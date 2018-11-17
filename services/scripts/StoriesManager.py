703fa41a607d1be1c69735af9ffe525c47b37337
Mick95 <michele.gennaioli@mail.polimi.it>
2018-11-17 17:45:35 +0100

    script in prova da modificare

---
+++ services/scripts/StoriesManager.py
@@ -0,0 +1,21 @@
+import glob
+import SpeakerManager
+
+storiesMatrix = []
+numberOfStories = 1
+
+class StoriesManager():
+
+    def __init__(self):
+
+        # creates a new list that contains all the path + name of the file of type mp3
+        list_of_file_audio = glob.glop("pathname *.mp3")
+        number_of_interaction = len(list_of_file_audio)
+
+        for stories in range(0, self.numberOfStories):
+            temporary_list = []
+            # for each story it creates a list of size number_of_interaction/numberOfStories
+            for part_of_story in range(0, number_of_interaction / self.numberOfStories):
+                # (stories*numberOfStories) is used to define the different value of list_of_file_audio
+                temporary_list.append(list_of_file_audio[part_of_story + (stories * self.numberOfStories)])
+            self.storiesMatrix.append(temporary_list)
