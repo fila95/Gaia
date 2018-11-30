e3ed365bae87a89d08164dd0d09d19b98351d2ad
Mick95 <michele.gennaioli@mail.polimi.it>
2018-11-30 14:49:18 +0100

    up

--- services/scripts/geki_2/sound/SpeakerManager.py
+++ services/scripts/geki_2/sound/SpeakerManager.py
@@ -1,3 +1,7 @@
+import threading
+import time
+from queue import Queue
+
 import pygame
 from pygame import mixer
 
@@ -6,20 +10,26 @@ class SpeakerManager:
 
     __speaker = mixer
 
-    def __init__(self):
+    def __init__(self, finishPlayingCallback):
 
         # initialize the speaker
+        self.__finishPlayingCallback = finishPlayingCallback
         self.__speaker.init()
         self.setVolume(1.0)
+        self.queue = Queue()
+        self.interrupt_event = threading.Event()
+
 
     def deinit(self):
         self.__speaker.quit()
 
     def stop(self):
         self.__speaker.music.stop()
+        self.interrupt_event.set()
 
     def pause(self):
         self.__speaker.music.pause()
+        self.interrupt_event.set()
 
     def playAudio(self, path=None):
         if self.__speaker.music is not None:
@@ -28,6 +38,9 @@ class SpeakerManager:
             else:
                 self.__speaker.music.load(path)
                 self.__speaker.music.play()
+            self.__handle_async(self.__checkPlaying, False)
+
+
 
     def setVolume(self, volume):
         if type(volume) is float:
@@ -35,3 +48,15 @@ class SpeakerManager:
 
     def isPlaying(self):
         return self.__speaker.music.get_busy()
+
+
+    def __handle_async(self, lfunc, interrupt=True):
+        # ql = self.queue.qsize()
+        if (interrupt == True and not self.queue.empty() and not self.interrupt_event.isSet()):
+            self.interrupt_event.set()
+        self.queue.put(lfunc)
+
+    def __checkPlaying(self):
+        while self.isPlaying():
+            time.sleep(0.1)
+        self.__finishPlayingCallback()
